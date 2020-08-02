e70d33ef24581d8121c7465e6dc8004810a71087
Shaik Salman Khursheed <45688914+shaikSalmankhursheed@users.noreply.github.com>
2020-08-02 14:09:44 +0530

    3rd commit
    1) Added Vote counter
    2) Added graph which updates live Votecounter
    3) A refresh will not perish the vote count, Added local storage functionality
    
    Future Items: Routes and Hide button

--- src/App.js
+++ src/App.js
@@ -2,42 +2,68 @@ import React, { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BsFillCaretUpFill } from "react-icons/bs";
 import axios from "axios";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
 
 import "./App.css";
 
 const App = () => {
   const [page, SetPage] = useState(0);
   const [apidata, Setapidata] = useState([]);
+  const [lstorage, Setlstorage] = useState([]);
 
   const getApidata = (page) => {
-    axios
-      .get(`https://hn.algolia.com/api/v1/search?&page=${page}`)
-      .then(function (response) {
-        // handle success
-        console.log(response.data.hits);
-        Setapidata(response.data.hits);
-      })
-      .catch(function (error) {
-        // handle error
-        console.log(error);
-      })
-      .finally(function () {
-        // empty for now
-      });
+    let localStorage1 = JSON.parse(localStorage.getItem("apidata"));
+    if (localStorage1 == null) {
+      axios
+        .get(`https://hn.algolia.com/api/v1/search?&page=${page}`)
+        .then(function (response) {
+          // handle success
+          // console.log(response.data.hits);
+          Setapidata(response.data.hits);
+          localStorage.setItem("apidata", JSON.stringify(response.data.hits));
+        })
+        .catch(function (error) {
+          // handle error
+          console.log(error);
+        })
+        .finally(function () {
+          // empty for now
+        });
+    } else {
+      Setapidata(localStorage1);
+    }
   };
-
+  const IncrementVote = (prep) => {
+    let obj = apidata.find((x) => x.objectID == prep);
+    console.log(obj);
+    let index = apidata.indexOf(obj);
+    console.log(index);
+    apidata.fill((obj.points = obj.points + 1), index, index++);
+    console.log(apidata);
+    localStorage.setItem("apidata", JSON.stringify(apidata));
+    // window.location.reload(false);
+    Setapidata(JSON.parse(localStorage.getItem("apidata")));
+  };
   const created_in_hours = (created) => {
     var today1 = new Date();
     // var created = "2013-05-16T20:57:17.000Z"; Testing variable
     var created_format = new Date(created);
     var difference = today1.getTime() - created_format.getTime();
     var hoursDifference = Math.floor(difference / 1000 / 60 / 60);
-    difference -= hoursDifference * 1000 * 60 * 60;
+
     return hoursDifference;
   };
   useEffect(() => {
     getApidata(page);
-    created_in_hours();
   }, [page]);
 
   return (
@@ -74,11 +100,16 @@ const App = () => {
                 </th>
                 <td>{d.points}</td>
                 <td>
-                  <span style={{ opacity: "0.5" }}>
+                  <span
+                    style={{ opacity: "0.5" }}
+                    onClick={() => {
+                      IncrementVote(d.objectID);
+                    }}
+                  >
                     <BsFillCaretUpFill />
                   </span>
                 </td>
-                <td className="text-left">
+                <td className="text-left" style={{ fontWeight: "bold" }}>
                   {d.title == "" || d.title == null
                     ? d._highlightResult.title?.value == null ||
                       d._highlightResult.title?.value == ""
@@ -102,8 +133,15 @@ const App = () => {
                       </a>
                     )}
                     by
-                    <span style={{ fontWeight: "bold" }}> {d.author}</span>
+                    <span style={{ fontWeight: "bold", color: "black" }}>
+                      {" "}
+                      {d.author}
+                    </span>
                     <span> {created_in_hours(d.created_at)} hours ago</span>
+                    <span>
+                      [<a style={{ cursor: "pointer", color: "black" }}>hide</a>
+                      ]
+                    </span>
                   </span>
                 </td>
               </tr>
@@ -142,6 +180,29 @@ const App = () => {
         </tfoot>
       </table>{" "}
       {page}
+      <hr className="orange" />
+      <ResponsiveContainer width={"99%"} height={300}>
+        <LineChart
+          width={1100}
+          height={200}
+          data={apidata}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="2 2" />
+          <XAxis dataKey="objectID" />
+          <YAxis />
+          <Tooltip />
+          <Legend verticalAlign="bottom" />
+
+          <Line type="monotone" dataKey="points" stroke="blue" name="Votes" />
+        </LineChart>
+      </ResponsiveContainer>
+      <hr className="orange" />
     </div>
   );
 };
